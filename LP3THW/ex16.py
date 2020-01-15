30b5d5dba8f6c60a43d58643e649a8d5127d451e
Mei Ninghang <meininghang@foxmail.com>
2020-01-15 22:28:08 +0800

    🐛 add ex16

--- LP3THW/ex16.py
+++ LP3THW/ex16.py
@@ -14,7 +14,7 @@ target = open(filename, 'w')
 print("Truncating the file. Goodbye!")
 target.truncate()
 
-print('Now I\'m going to ask you for three lines.)
+print('Now I\'m going to ask you for three lines.')
 
 line1 = input("line 1: ")
 line2 = input("line 2: ")
