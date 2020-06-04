6de202f9224f5b304431d60e4f55f8249d94a808
梅宁航 <zzumnh@163.com>
2020-06-04 11:59:08 +0800

    6.3 总结

---
+++ C++基础学习/hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //头文件,控制输入输出
+#include <cstdlib>//标准函数库
+
+using namespace std; //使用标准链接库的命名空间std
+int main()//main函数为c++主程序的进入点,int代表数据类型为整型
+{
+    cout<<"我的第一个C++程序"<<"\n";//cout代表输出,endl代表换行,"\n"也行
+    return 0;//因为主程序被声明为整型,所以必须有返回值
+
+}
