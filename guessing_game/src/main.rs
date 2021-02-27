de40ca4794cc5717d4ce30c9a1d79524d0d0ad2d
meininghang <meininghang@yeah.net>
2021-02-27 11:52:05 +0800

    rust  go!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,19 +1,35 @@
-use std::io;//引入标准库中的io模块
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
-        use rand::Rng;//Rng是一个特征定义随机数实现的方法集合
 
-        fn main() {//fn声明函数，()为空表明函数内部不含参数，{}内为函数体
-                println!("Guess the number!");//！表明为宏，负责打印输出
+fn main(){
+    println!("Guess the number!");
 
-                let secret_number = 
-rand::thread_rng().gen_range(1,101);//前者返回随机数生成器函数，后者负责随机数生成方法
-                println!("The secret_number is: {}",secret_number);
-        println!("Please input your guess.");//作用同上
+    let secret_number = rand::thread_rng().gen_range(1,101);
 
-        let mut guess = String::new();//存储用户输入内容，mut表明guess为可变变量，右边表明为输入string内容，new为关联函数
+    loop {
+        println!("Please input your guess.");
 
-        io::stdin().read_line(&mut guess)//调用io模块中的关联函数stdin，&表示参数为引用
-            .expect("Failed to read line");
-    
-        println!("You guessed: {}", guess);//打印，并将guess参数值输出
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}",guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            
+        }
+    }
 }
---
+++ guessing_game/target/debug/deps/guessing_game-656b8695e2ca3f7a.d
@@ -0,0 +1,5 @@
+/Users/meininghang/Python-Learning/guessing_game/target/debug/deps/guessing_game-656b8695e2ca3f7a: src/main.rs
+
+/Users/meininghang/Python-Learning/guessing_game/target/debug/deps/guessing_game-656b8695e2ca3f7a.d: src/main.rs
+
+src/main.rs:
