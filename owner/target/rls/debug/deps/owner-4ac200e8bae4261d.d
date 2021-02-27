e6a91e064fdcef00584a28850d4bec8c7b271cd8
meininghang <meininghang@yeah.net>
2021-02-27 23:58:11 +0800

    ownership part1

---
+++ owner/target/rls/debug/deps/owner-4ac200e8bae4261d.d
@@ -0,0 +1,5 @@
+/Users/meininghang/Python-Learning/owner/target/rls/debug/deps/owner-4ac200e8bae4261d.rmeta: src/main.rs
+
+/Users/meininghang/Python-Learning/owner/target/rls/debug/deps/owner-4ac200e8bae4261d.d: src/main.rs
+
+src/main.rs:
---
+++ owner/target/rls/debug/deps/owner-f099cd22c5c3940a.d
@@ -0,0 +1,5 @@
+/Users/meininghang/Python-Learning/owner/target/rls/debug/deps/owner-f099cd22c5c3940a.rmeta: src/main.rs
+
+/Users/meininghang/Python-Learning/owner/target/rls/debug/deps/owner-f099cd22c5c3940a.d: src/main.rs
+
+src/main.rs:
