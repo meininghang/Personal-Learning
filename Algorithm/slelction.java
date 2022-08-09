bce9c23c5a64bff40e7d4c1955e547154b5f3302
Ninghang.Mei <ninghang.mei@gmail.com>
2022-08-09 15:14:42 +0400

    test again

---
+++ Algorithm/slelction.java
@@ -0,0 +1,10 @@
+//package com.mashibing;
+public class slelction {
+    public static void main(String[] args) {
+        int[] arr = {5,3,6,8,1,7,9,4,2};
+
+        for(int i=0; i< arr.length; i++){
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
