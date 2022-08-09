738d9967cf474a4e00ea605b21da54e9a5251d1c
Ninghang.Mei <ninghang.mei@gmail.com>
2022-08-09 16:03:39 +0400

    update selection

--- Algorithm/slelction.java
+++ Algorithm/slelction.java
@@ -3,7 +3,11 @@ public class slelction {
     public static void main(String[] args) {
         int[] arr = {5,3,6,8,1,7,9,4,2};
 
-        for(int i=0; i< arr.length; i++){
+        int minPos = 0;
+
+        
+
+        for(int i=0; i< arr.le ngth; i++){
             System.out.println(arr[i] + " ");
         }
     }
