98af6bc9b5704843c383a806ac77d4a50e092a47
Ninghang.Mei <ninghang.mei@gmail.com>
2022-08-19 01:54:20 +0400

    test

--- Python for Everybody/max_pairwise_product.py
+++ Python for Everybody/max_pairwise_product.py
@@ -7,5 +7,5 @@ def max_pairwise_product(numbers):
 if __name__ == '__main__':
     _ = int(input())
     input_numbers = list(map(int, input().split()))
-    print(max_pairwise_product(input_numbers))
+    print(max_pairwise_product(input_numbers)) 
 
