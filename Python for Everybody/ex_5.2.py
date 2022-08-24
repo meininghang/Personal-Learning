b1f389f7df974db5aa76052e7ccc1c34cd7e060b
Ninghang.Mei <ninghang.mei@gmail.com>
2022-08-24 13:49:38 +0400

    update homework

---
+++ Python for Everybody/ex_5.2.py
@@ -0,0 +1,24 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num = int(num)
+        
+        if largest is None:
+            largest = num
+        elif largest < num:
+            largest = num
+        
+        if smallest is None:
+                smallest = num
+        elif smallest > num:
+                smallest = num
+    except:
+        print('Invalid input')
+        continue
+
+print("Maximum", largest)
+print("Minimum", smallest)
