4f4b4c116fe5d9d0f170224c8f3d60acf61531ae
Ninghang.Mei <ninghang.mei@gmail.com>
2023-03-24 14:20:32 +0400

    再次重新开始

--- C_Learning/printf_ex.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    printf("**************\n");
-    printf("猴博士贼溜!\n");
-    printf("***************\n");
-}
--- Python for Everybody/ex_6_2.py
+++
@@ -1,3 +0,0 @@
-text = "X-DSPAM-Confidence:    0.8475"
-found = text.find(':')
-print(float(text[found+1:]))
