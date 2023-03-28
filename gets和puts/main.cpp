2e327abd710eab41e0bad1df07dc52d19640b1d6
Ninghang.Mei <ninghang.mei@gmail.com>
2023-03-28 16:11:53 +0400

    反复在代码中学习！

--- gets和puts/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-int main()
-{
-    char c[20];//字符数组的数组名里存的就是字符数组的起始地址,类型是字符指针
-    fgets(c,20,stdin);//一次读取一行
-    puts(c);//等价于printf
-    return 0;
-}
--- 函数的嵌套调用/func.h
+++
@@ -1,9 +0,0 @@
-#ifndef PERSONAL_LEARNING_FUNC_H
-#define PERSONAL_LEARNING_FUNC_H
-
-#endif //PERSONAL_LEARNING_FUNC_H
-//头文件中放置函数声明和常量
-#include <stdio.h>
-
-int printstar(int i);//函数声明
-void print_message();//函数声明
