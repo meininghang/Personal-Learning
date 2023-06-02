33b8ddaefee3ba747e019d96b55b8c5677d4ed6d
Ninghang.Mei <ninghang.mei@gmail.com>
2023-06-02 05:42:13 +0400

    幸福人生=编程+英语

---
+++ twitter_cal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void cal(int views, int engagement){
+    int incentive;
+    if (views <= 500)
+    {
+        incentive = 20;
+    }
+    
+}
+
+int main(){
+
+    return 0;
+}
